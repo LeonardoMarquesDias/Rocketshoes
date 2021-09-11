edd8ce0d00be943918dffc9a88b7b2ca42478a06
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-09-11 16:04:38 +0100

    teste passed Card

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -30,11 +30,11 @@ const Cart = (): JSX.Element => {
     formatPrice(
       cart.reduce((sumTotal, product) => {
         return sumTotal + product.price * product.amount
-    }, 0)
-  )
+      }, 0)
+    )
 
   function handleProductIncrement(product: Product) {
-    updateProductAmount({ productId: product.id, amount: product.amount +1 })
+    updateProductAmount({ productId: product.id, amount: product.amount + 1 })
   }
 
   function handleProductDecrement(product: Product) {
@@ -60,51 +60,51 @@ const Cart = (): JSX.Element => {
         <tbody>
           {cartFormatted.map(product => (
             <tr key={product.id} data-testid="product">
-            <td>
-              <img src={product.image} alt={product.title} />
-            </td>
-            <td>
-              <strong>{product.title}</strong>
-              <span>{product.priceFormatted}</span>
-            </td>
-            <td>
-              <div>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button
+                    type="button"
+                    data-testid="decrement-product"
+                    disabled={product.amount <= 1}
+                    onClick={() => handleProductDecrement(product)}
+                  >
+                    <MdRemoveCircleOutline size={20} />
+                  </button>
+                  <input
+                    type="text"
+                    data-testid="product-amount"
+                    readOnly
+                    value={product.amount}
+                  />
+                  <button
+                    type="button"
+                    data-testid="increment-product"
+                    onClick={() => handleProductIncrement(product)}
+                  >
+                    <MdAddCircleOutline size={20} />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subTotal}</strong>
+              </td>
+              <td>
                 <button
                   type="button"
-                  data-testid="decrement-product"
-                  disabled={product.amount <= 1}
-                  onClick={() => handleProductDecrement(product)}
+                  data-testid="remove-product"
+                  onClick={() => handleRemoveProduct(product.id)}
                 >
-                  <MdRemoveCircleOutline size={20} />
+                  <MdDelete size={20} />
                 </button>
-                <input
-                  type="text"
-                  data-testid="product-amount"
-                  readOnly
-                  value={product.amount}
-                />
-                <button
-                  type="button"
-                  data-testid="increment-product"
-                  onClick={() => handleProductIncrement(product)}
-                >
-                  <MdAddCircleOutline size={20} />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>{product.subTotal}</strong>
-            </td>
-            <td>
-              <button
-                type="button"
-                data-testid="remove-product"
-                onClick={() => handleRemoveProduct(product.id)}
-              >
-                <MdDelete size={20} />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
           ))}
         </tbody>
       </ProductTable>
@@ -114,7 +114,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
